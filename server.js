e5bf9adc886f90ccc7c9ce9ca2933102f14e24c2
saisameerd <saisameer.d@gmail.com>
2022-01-26 15:08:38 +0530

    Created server and index.html

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const server = require('http').createServer(app);
+
+app.use(express.static(path.join(__dirname,'/public')));
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+})
+
+
+
+server.listen(3000)
